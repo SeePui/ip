6b0f47be2c1887f169650b1d9ad0f5a5aa875179
SeePui <ngseepui95@gmail.com>
2026-01-21 14:16:01 +0800

    Rename chatbot and implement initial skeleton

---
+++ src/main/java/Pie.java
@@ -0,0 +1,12 @@
+public class Pie {
+    static String chatbotName = "Pie";
+    static String line = "---------------------------------";
+
+    public static void main(String[] args) {
+        System.out.println(line);
+        System.out.println("Hello! I'm " + chatbotName + "\nWhat can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
