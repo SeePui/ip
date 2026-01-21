191ebe99dc982d3a18766148a10726a3d9fa58b4
SeePui <ngseepui95@gmail.com>
2026-01-22 04:00:20 +0800

    Add mark and unmark tasks as done functionality

--- src/main/java/Pie.java
+++ src/main/java/Pie.java
@@ -1,35 +1,36 @@
 import java.util.*;
 
 public class Pie {
-    private static String chatbotName = "Pie";
+    private static final String chatbotName = "Pie";
     private static final String line = "\n________________________________________________________\n";
+    private static List<Task> taskList = new ArrayList<>();
 
     public static void main(String[] args) {
         startMessage();
 
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
-
-        List<Task> taskList = new ArrayList<>();
+        String input = scanner.nextLine();
 
         while (true) {
-            if (command.equals("bye")) {
+            String[] commands = input.split(" ");
+            String command = commands[0];
+
+            if (input.equals("bye")) {
                 byeMessage();
                 break;
-            } else if(command.equals("list")) {
-                System.out.println(line);
-                for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println(i+1 + ". " + taskList.get(i).getDescription());
-                    if (i  == taskList.size() - 1) {
-                        System.out.println(line);
-                    }
-                }
+            } else if (input.equals("list")) {
+                printTasks();
+            } else if (command.equals("mark")) {
+                int tempTaskNum = Integer.parseInt(commands[1]) - 1;
+                markTask(tempTaskNum);
+            } else if (command.equals("unmark")) {
+                int tempTaskNum = Integer.parseInt(commands[1]) - 1;
+                unmarkTask(tempTaskNum);
+            } else {
+                addTask(input);
             }
-            else {
-                System.out.println(line + "added: " + command + line);
-                taskList.add(new Task(command));
-            }
-            command = scanner.nextLine();
+
+            input = scanner.nextLine();
         }
         scanner.close();
     }
@@ -40,5 +41,37 @@ public class Pie {
 
     private static void byeMessage() {
         System.out.println(line + "Bye. Hope to see you again soon!" + line);
+    }
+
+    private static void addTask(String command) {
+        System.out.println(line + "added: " + command + line);
+        taskList.add(new Task(command));
+    }
+
+    private static void printTasks() {
+        System.out.println(line + "Here are the tasks in your list:");
+        for (int i = 0; i < taskList.size(); i++) {
+            Task task = taskList.get(i);
+            System.out.println(i + 1 + ".["
+                    + task.getStatusIcon() + "] "
+                    + task.getDescription());
+            if (i == taskList.size() - 1) {
+                System.out.println(line);
+            }
+        }
+    }
+
+    private static void markTask(int taskNumber) {
+        taskList.get(taskNumber).markDone();
+        Task task = taskList.get(taskNumber);
+        System.out.println(line + "Nice! I've marked this task as done:\n"
+                + "   [" + task.getStatusIcon() + "] " + task.getDescription() + line);
+    }
+
+    private static void unmarkTask(int taskNumber) {
+        taskList.get(taskNumber).unmarkDone();
+        Task task = taskList.get(taskNumber);
+        System.out.println(line + "OK, I've marked this task as not done yet:\n"
+                + "   [" + task.getStatusIcon() + "] " + task.getDescription() + line);
     }
 }
