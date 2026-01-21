178fea6625a2d5e648c009320d22b6434084ff20
SeePui <ngseepui95@gmail.com>
2026-01-21 20:11:25 +0800

    Add echo commands

--- src/main/java/Pie.java
+++ src/main/java/Pie.java
@@ -1,12 +1,23 @@
+import java.util.Scanner;
+
 public class Pie {
     static String chatbotName = "Pie";
-    static String line = "---------------------------------";
+    static String line = "\n________________________________________________________\n";
 
     public static void main(String[] args) {
-        System.out.println(line);
-        System.out.println("Hello! I'm " + chatbotName + "\nWhat can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(line + "Hello! I'm " + chatbotName + "\nWhat can I do for you?" + line);
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        while (true) {
+            if (command.equals("bye")) {
+                System.out.println(line + "Bye. Hope to see you again soon!" + line);
+                break;
+            } else {
+                System.out.println(line + command + line);
+                command = scanner.nextLine();
+            }
+        }
+        scanner.close();
     }
 }
