fcf3309dc366ec32da1dd1bb1c4f40fc13d63bef
SeePui <ngseepui95@gmail.com>
2026-01-29 23:24:38 +0800

    Edit JUnit tests for Parser and TaskList

--- src/test/java/pie/task/TaskListTest.java
+++ src/test/java/pie/task/TaskListTest.java
@@ -1,4 +1,37 @@
 package pie.task;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TaskListTest {
+    @Test
+    public void markTask_validIndex_marksTaskAsDone() {
+        TaskList taskList = new TaskList();
+        taskList.addTodo("return book");
+        Task markedTask = taskList.markTask(0);
+
+        assertTrue(markedTask.getIsDone());
+    }
+
+    @Test
+    public void markTask_invalidIndex_exceptionThrown() {
+        TaskList tasks = new TaskList();
+
+        assertThrows(IndexOutOfBoundsException.class, () ->
+                tasks.markTask(0)
+        );
+    }
+
+    @Test
+    public void addTodo_validDescription_todoAdded() {
+        TaskList taskList = new TaskList();
+        Task task = taskList.addTodo("read book");
+
+        assertNotNull(task);
+        assertInstanceOf(Todo.class, task);
+        assertEquals("read book", task.getDescription());
+        assertEquals(1, taskList.getSize());
+        assertEquals(task, taskList.getAllTasks().get(0));
+    }
 }
