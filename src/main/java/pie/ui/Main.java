6f6e07a094bc8a44983a1c1d37e269d93d83c12a
SeePui <ngseepui95@gmail.com>
2026-02-17 02:26:04 +0800

    Add icon for send button

--- src/main/java/pie/ui/Main.java
+++ src/main/java/pie/ui/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
             stage.setScene(scene);
             stage.setTitle("Pie Chatbot");
             stage.setMinHeight(250);
-            stage.setMinWidth(420);
+            stage.setMinWidth(400);
             fxmlLoader.<MainWindow>getController().setPie(pie); // inject the Pie instance
             stage.show();
         } catch (IOException e) {
